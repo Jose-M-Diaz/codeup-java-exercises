484873ff56acbe7ca916d52b60354d01d5865c5a
Jose Diaz <Diaz.M.Jose68@gmail.com>
2021-12-14 15:43:13 -0600

    completed syntax variables java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,38 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello ");
-        System.out.print("World");
+//        System.out.print("Hello ");
+//        System.out.print("World");
+
+//        int myFavoriteNumber = 13;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "I like to party";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14f;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three"; //cant convert string to int
+//        Object o = theNumberThree;      // Object can be used as generic for string so its just repeating same info?
+//        int three = (int) o;
+//        System.out.println(three);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
     }
 }
